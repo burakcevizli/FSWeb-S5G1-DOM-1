e02d23dda6600349886d7143c98b19999df97cc0
KrieqqeR <burakcevizli@gmail.com>
2023-07-24 19:48:56 +0300

    Ödev yapıldı

--- src/index.js
+++ src/index.js
@@ -54,12 +54,12 @@ aLinks[1].textContent = "Ürünler"
 aLinks[2].textContent = "Vizyon"
 aLinks[3].textContent = "Özellikler"
 aLinks[4].textContent = "Hakkımızda"
-aLinks[5].textContent = "İletişimler"
+aLinks[5].textContent = "İletişim"
 
 
 const firstImg = document.getElementById("logo-img")
 
-firstImg.setAttribute("src", "../mocks/img/logo.png")
+firstImg.setAttribute("src", "http://localhost:9000/img/logo.png")
 
 
 const domMuk = document.querySelector(".cta-text > h1")
@@ -72,7 +72,7 @@ domButton.textContent = "Başlayın"
 
 const yuvarlakKodResmi = document.getElementById("cta-img")
 
-yuvarlakKodResmi.setAttribute("src", "../mocks/img/cta.png")
+yuvarlakKodResmi.setAttribute("src", "http://localhost:9000/img/cta.png")
 
 const ozelliklerKismiH4 = document.querySelectorAll(".main-content > .top-content > .text-content > h4")
 
@@ -89,6 +89,43 @@ ozelliklerKismiP[1].textContent = "Hakkında içeriği elementum magna eros, ac 
 
 const middleImg = document.getElementById("middle-img" )
 
-middleImg.setAttribute("src","../mocks/img/accent.png")
+middleImg.setAttribute("src","http://localhost:9000/img/accent.png")
 
 
+const middleKismiH4 = document.querySelectorAll(".main-content > .bottom-content > .text-content > h4")
+
+const middleKismiP = document.querySelectorAll(".main-content > .bottom-content > .text-content > p")
+
+
+middleKismiH4[0].textContent = "Servisler"
+
+middleKismiP[0].textContent = "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl.Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
+middleKismiH4[1].textContent = "Ürünler"
+
+middleKismiP[1].textContent = "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+middleKismiH4[2].textContent = "Vizyon"
+
+middleKismiP[2].textContent = "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+
+
+const contactKismiH = document.querySelectorAll(".contact > h4")
+
+const contactKismiP = document.querySelectorAll(".contact > p")
+
+contactKismiH[0].textContent = "İletişim"
+
+contactKismiP[0].textContent = "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye"
+contactKismiP[1].textContent = "+90 (123) 456-7899"
+contactKismiP[2].textContent = "satis@birsirketsitesi.com.tr"
+
+
+
+const footerlink = document.querySelector("footer > a")
+
+footerlink.setAttribute("class","bold")
+
+footerlink.textContent = "Copyright Bir Şirket Sitesi 2022"
